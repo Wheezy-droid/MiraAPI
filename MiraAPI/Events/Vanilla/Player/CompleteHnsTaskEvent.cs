089d2c1ea541afc92df120e0e6cfd7764be328f2
Atony <75191600+AtonyGit@users.noreply.github.com>
2025-11-23 12:01:41 -0600

    Add Hide and seek task event

---
+++ MiraAPI/Events/Vanilla/Player/CompleteHnsTaskEvent.cs
@@ -0,0 +1,28 @@
+﻿namespace MiraAPI.Events.Vanilla.Player;
+
+/// <summary>
+/// The event that is invoked when a player completes a task within Hide and Seek. Task cannot be cancelled, but timer changes can.
+/// </summary>
+public class CompleteHnsTaskEvent : MiraCancelableEvent
+{
+    /// <summary>
+    /// Gets the instance of the <see cref="PlayerControl"/>.
+    /// </summary>
+    public PlayerControl Player { get; }
+
+    /// <summary>
+    /// Gets the instance of the <see cref="PlayerTask"/> that the player completed.
+    /// </summary>
+    public PlayerTask Task { get; }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="CompleteHnsTaskEvent"/> class.
+    /// </summary>
+    /// <param name="player">The player who completed the task.</param>
+    /// <param name="task">The task that the player completed.</param>
+    public CompleteHnsTaskEvent(PlayerControl player, PlayerTask task)
+    {
+        Player = player;
+        Task = task;
+    }
+}
---
+++ MiraAPI/Patches/Events/HnsFinishTaskPatch.cs
@@ -0,0 +1,43 @@
+﻿
+using HarmonyLib;
+using MiraAPI.Events;
+using MiraAPI.Events.Vanilla.Player;
+
+namespace MiraAPI.Patches.Events;
+
+[HarmonyPatch(typeof(HideAndSeekManager))]
+public static class HnsFinishTaskPatch
+{
+    [HarmonyPrefix]
+    [HarmonyPatch(nameof(HideAndSeekManager.FinishTask))]
+    public static bool FinishTask(HideAndSeekManager __instance, PlayerTask task)
+    {
+        NormalPlayerTask normalPlayerTask = task as NormalPlayerTask;
+        if (normalPlayerTask != null)
+        {
+            var completeTaskEvent = new CompleteHnsTaskEvent(task.Owner, task);
+            MiraEventManager.InvokeEvent(completeTaskEvent);
+            if (completeTaskEvent.IsCancelled)
+            {
+                return false;
+            }
+            switch (normalPlayerTask.Length)
+            {
+                case NormalPlayerTask.TaskLength.None:
+                case NormalPlayerTask.TaskLength.Common:
+                    __instance.LogicFlowHnS.OnTaskComplete(__instance.LogicOptionsHnS.GetCommonTaskTimeValue());
+                    break;
+                case NormalPlayerTask.TaskLength.Short:
+                    __instance.LogicFlowHnS.OnTaskComplete(__instance.LogicOptionsHnS.GetShortTaskTimeValue());
+                    break;
+                case NormalPlayerTask.TaskLength.Long:
+                    __instance.LogicFlowHnS.OnTaskComplete(__instance.LogicOptionsHnS.GetLongTaskTimeValue());
+                    break;
+            }
+
+            SoundManager.Instance.PlaySoundImmediate(__instance.TaskFinishedSound, false, 1f, 1f, null);
+        }
+
+        return false;
+    }
+}
