4c4bab40cd0c5a5204634a0dbf1dd08709f25f1d
Wheezy-droid <abdulmohsinmaisara@gmail.com>
2026-04-30 15:27:31 +0400

    Update ExamplePlugin.cs

--- MiraAPI.Example/ExamplePlugin.cs
+++ MiraAPI.Example/ExamplePlugin.cs
@@ -1,12 +1,11 @@
 using HarmonyLib;
-using System;
 
 namespace MiraAPI.Example
 {
     [HarmonyPatch]
     public class ExamplePlugin
     {
-        // This is the "Selector" that lets you choose the game mode in the lobby
+        // 1. Adds the Mode Selector to the Lobby Menu
         [HarmonyPatch(typeof(global::MiraAPI.Options.TargetOptions), nameof(global::MiraAPI.Options.TargetOptions.Initialize))]
         [HarmonyPostfix]
         public static void SetupMenu()
@@ -14,6 +13,7 @@ namespace MiraAPI.Example
             global::MiraAPI.Options.TargetOptions.AddString("Game Mode", new[] { "Normal", "SNS", "Freeze Tag" }, "Normal");
         }
 
+        // 2. Changes the game rules based on what you picked
         [HarmonyPatch(typeof(global::MiraAPI.Options.GameOptionsManager), nameof(global::MiraAPI.Options.GameOptionsManager.Load))]
         [HarmonyPostfix]
         public static void ApplySelectedMode()
@@ -21,15 +21,15 @@ namespace MiraAPI.Example
             var options = global::MiraAPI.Options.GameOptionsManager.Instance.currentVars;
             if (options == null) return;
 
-            // --- SNS MODE LOGIC ---
+            // SNS Rules
             options.NumShapeshifters = 3;
             options.ShapeshifterChance = 100;
             options.ReportDistance = 0f;
             options.NumEmergencyMeetings = 0;
 
-            // --- FREEZE TAG LOGIC (TikTok Rules) ---
-            options.KillDistance = 0; // Must touch them to 'freeze'
-            options.CanVent = false;   // Impostors/Taggers can't vent
+            // Freeze Tag Rules (TikTok Rules)
+            options.KillDistance = 0; // Touch to freeze
+            options.CanVent = false;   // Taggers can't vent
         }
     }
 
@@ -39,7 +39,7 @@ namespace MiraAPI.Example
         [HarmonyPostfix]
         public static void Postfix(PlayerControl __instance)
         {
-            // If someone is 'killed' in Freeze Tag, they just stop moving instead of dying
+            // If they are 'dead' (tagged), they stop moving
             if (__instance.Data.IsDead && !__instance.Data.IsImpostor)
             {
                 __instance.MyPhysics.Speed = 0; 
