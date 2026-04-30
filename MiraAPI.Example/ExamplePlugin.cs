cd2813b46ae7c9accc9aeaecc203dd41b22e3693
Wheezy-droid <abdulmohsinmaisara@gmail.com>
2026-04-30 14:56:37 +0400

    Sns mod for sns

--- MiraAPI.Example/ExamplePlugin.cs
+++ MiraAPI.Example/ExamplePlugin.cs
@@ -1,21 +1,16 @@
 using HarmonyLib;
-using MiraAPI;
-using MiraAPI.Options;
-using MiraAPI.Roles;
 using System;
 
 namespace MiraAPI.Example
 {
     [HarmonyPatch]
-    public class ExamplePlugin : MiraPlugin
+    public class ExamplePlugin : global::MiraAPI.MiraPlugin
     {
         public override string OptionsTitleText => "Wheezy SNS Mode";
 
         public override void Load()
         {
-            // Use the full path to make it easier for the compiler
-            var options = MiraAPI.Options.GameOptionsManager.Instance.currentVars;
-            
+            var options = global::MiraAPI.Options.GameOptionsManager.Instance.currentVars;
             if (options != null)
             {
                 options.SabotageCooldown = 20f; 
@@ -25,19 +20,6 @@ namespace MiraAPI.Example
                 options.ShapeshifterChance = 100;
             }
             HarmonyLib.Harmony.PatchAll();
-        }
-    }
-
-    [HarmonyPatch(typeof(ChatController), nameof(ChatController.SendChat))]
-    public static class ChatPatch
-    {
-        [HarmonyPostfix]
-        public static void Postfix(ChatController __instance)
-        {
-            if (__instance != null && __instance.TextArea.text.ToLower().Contains("/r"))
-            {
-                __instance.AddChat(PlayerControl.LocalPlayer, "SNS: Shift-kill only. Comms only. No reports.");
-            }
         }
     }
 }
